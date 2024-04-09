d9bc92b51988c468a765b32cbe6b0e5f48b57e48
crocmons <ilmasalsabil1006@gmail.com>
2024-04-09 21:51:09 +0600

    initial commit

---
+++ mydb.py
@@ -0,0 +1,16 @@
+import pymysql
+
+# Connect to MySQL server
+database = pymysql.connect(
+    host='127.0.0.1',
+    user='root',
+    password='ilmasalsabil12345',
+)
+
+# Create a cursor object
+cursorObject = database.cursor()
+
+# Create a database
+cursorObject.execute("CREATE DATABASE CRM_DB")
+
+print("All done!")
